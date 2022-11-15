ed6ff4fbbea3f6c36539e73ea36f0652df724e0f
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-11-15 15:52:53 -0600

    First push

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+//This code was to show how to link HTML and JavaScript sheets together.
+let js = 'amazing';
+if(js === 'amazing') alert('JavaScript is Fun!')
+console.log("Hello");
+console.log(40 + 8 + 23 - 10);
