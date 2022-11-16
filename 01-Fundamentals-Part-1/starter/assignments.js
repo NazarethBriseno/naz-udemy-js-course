59296ec95ad63b27bfe1defed562f3a855cef7da
Nazareth-1 <nazarethbriseno@gmail.com>
2022-11-16 15:50:09 -0600

    Let Var Const Exercises

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+
+//Declaring a variable with the value of United States
+let country = "United States";
+//Declaring a variable with the value of North America
+let continent = "North America";
+//Declaring a variable called population with the value of 339000000
+let population = 339000000;
+//Logging the values of country, continent and population to the console
+console.log(country);
+console.log(continent);
+console.log(population);
+
--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,27 @@
 //This code was to show how to link HTML and JavaScript sheets together.
 let js = 'amazing';
-if(js === 'amazing') alert('JavaScript is Fun!')
-console.log("Hello");
 console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Jonas';
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+console.log(typeof 23);
+//To change a variable's value that has already put declared do NOT use the let keyword again
+javaScriptIsFun = 'Yes!';
+
+let isIsland = false;
+let language;
+console.log(isIsland);
+console.log(language);
+
+language = "English";
+
+
