a8c2b190c26f3895f639b3e796d7bd9cfab74d85
Nazareth Briseno <nazarethbriseno@gmail.com>
2022-11-20 12:34:57 -0600

    Completed If statements udemy exercises

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,27 +1,94 @@
-//This code was to show how to link HTML and JavaScript sheets together.
-let js = 'amazing';
-console.log(40 + 8 + 23 - 10);
+// //This code was to show how to link HTML and JavaScript sheets together.
+// let js = 'amazing';
+// console.log(40 + 8 + 23 - 10);
+//
+// console.log('Jonas');
+// console.log(23);
+//
+// let firstName = 'Jonas';
+//
+//
+// let javaScriptIsFun = true;
+// console.log(javaScriptIsFun);
+//
+// console.log(typeof true);
+// console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// //To change a variable's value that has already put declared do NOT use the let keyword again
+// javaScriptIsFun = 'Yes!';
+//
+// let isIsland = false;
+// let language;
+// console.log(isIsland);
+// console.log(language);
+//
+// language = "English";
 
-console.log('Jonas');
-console.log(23);
+// let markMass = 78;
+// let markHeight = 1.69;
+let markMass = 95;
+let markHeight = 1.88;
 
-let firstName = 'Jonas';
+let johnMass = 85;
+let johnHeight = 1.76;
 
+let markBMI = markMass/ markHeight **2;
+let johnBMI = johnMass/johnHeight **2;
 
-let javaScriptIsFun = true;
-console.log(javaScriptIsFun);
-
-console.log(typeof true);
-console.log(typeof javaScriptIsFun);
-console.log(typeof 23);
-//To change a variable's value that has already put declared do NOT use the let keyword again
-javaScriptIsFun = 'Yes!';
-
-let isIsland = false;
-let language;
-console.log(isIsland);
-console.log(language);
-
-language = "English";
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
+if(markBMI > johnBMI){
+    console.log(`Mark's BMI is ${markBMI}, which is higher than John's BMI, ${johnBMI}`);
+} else {
+    console.log(`John's BMI is ${johnBMI}, which is higher than Mark's BMI, ${markBMI}`);
+}
 
 
+
+
+
+console.log(`Hello i'm naz`);
+//Backticks can create multiple lines without using \n
+console.log(`
+Hello 
+Im Naz
+            `);
+
+const age = 19;
+const isOldEnough = age >= 18;
+
+if(isOldEnough){
+    console.log(`Sarah can start driving because her age is ${age} 🚗`);
+} else {
+    console.log(`Sarah cannot drive because she is only ${age}`);
+}
+
+let birthYear = 1998;
+let century;
+if (birthYear <= 2000){
+    century = 20;
+} else {
+    century =21;
+}
+console.log(century);
+
+let myArray = [1, 2, 3, 4];
+console.log(myArray)
+
+console.log(Number(myArray));
+
+//Coding challenge #2
+
+let dolphinsAvg = (96 + 108 + 89)/3;
+let koalasAvg = (88 + 100 +  110)/3;
+console.log(dolphinsAvg);
+console.log(koalasAvg);
+if(dolphinsAvg > koalasAvg && dolphinsAvg >= 100){
+    console.log("Dolphins Win!")
+} else if (koalasAvg > dolphinsAvg && koalasAvg >= 100) {
+    console.log("Koalas Win!")
+} else if (koalasAvg === dolphinsAvg && koalasAvg >= 100 && dolphinsAvg >=100){
+    console.log("Its a draw!")
+} else {
+    console.log("No one wins the trophy!")
+}
