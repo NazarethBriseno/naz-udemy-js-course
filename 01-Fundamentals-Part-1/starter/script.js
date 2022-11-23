b866eec86114ff1f668a56db419425d9424d8957
Nazareth-1 <nazarethbriseno@gmail.com>
2022-11-23 17:26:50 -0600

    Udemy Coding Challenge #4

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -26,69 +26,102 @@
 
 // let markMass = 78;
 // let markHeight = 1.69;
-let markMass = 95;
-let markHeight = 1.88;
-
-let johnMass = 85;
-let johnHeight = 1.76;
-
-let markBMI = markMass/ markHeight **2;
-let johnBMI = johnMass/johnHeight **2;
-
-let markHigherBMI = markBMI > johnBMI;
-console.log(markHigherBMI);
-if(markBMI > johnBMI){
-    console.log(`Mark's BMI is ${markBMI}, which is higher than John's BMI, ${johnBMI}`);
-} else {
-    console.log(`John's BMI is ${johnBMI}, which is higher than Mark's BMI, ${markBMI}`);
-}
-
-
+// let markMass = 95;
+// let markHeight = 1.88;
+//
+// let johnMass = 85;
+// let johnHeight = 1.76;
+//
+// let markBMI = markMass/ markHeight **2;
+// let johnBMI = johnMass/johnHeight **2;
+//
+// let markHigherBMI = markBMI > johnBMI;
+// console.log(markHigherBMI);
+// if(markBMI > johnBMI){
+//     console.log(`Mark's BMI is ${markBMI}, which is higher than John's BMI, ${johnBMI}`);
+// } else {
+//     console.log(`John's BMI is ${johnBMI}, which is higher than Mark's BMI, ${markBMI}`);
+// }
+//
 
 
 
 console.log(`Hello i'm naz`);
 //Backticks can create multiple lines without using \n
-console.log(`
-Hello 
-Im Naz
-            `);
-
-const age = 19;
-const isOldEnough = age >= 18;
-
-if(isOldEnough){
-    console.log(`Sarah can start driving because her age is ${age} 🚗`);
-} else {
-    console.log(`Sarah cannot drive because she is only ${age}`);
-}
-
-let birthYear = 1998;
-let century;
-if (birthYear <= 2000){
-    century = 20;
-} else {
-    century =21;
-}
-console.log(century);
-
-let myArray = [1, 2, 3, 4];
-console.log(myArray)
-
-console.log(Number(myArray));
+// console.log(`
+// Hello
+// Im Naz
+//             `);
+//
+// const age = 19;
+// const isOldEnough = age >= 18;
+//
+// if(isOldEnough){
+//     console.log(`Sarah can start driving because her age is ${age} 🚗`);
+// } else {
+//     console.log(`Sarah cannot drive because she is only ${age}`);
+// }
+//
+// let birthYear = 1998;
+// let century;
+// if (birthYear <= 2000){
+//     century = 20;
+// } else {
+//     century =21;
+// }
+// console.log(century);
+//
+// let myArray = [1, 2, 3, 4];
+// console.log(myArray)
+//
+// console.log(Number(myArray));
 
 //Coding challenge #2
 
-let dolphinsAvg = (96 + 108 + 89)/3;
-let koalasAvg = (88 + 100 +  110)/3;
-console.log(dolphinsAvg);
-console.log(koalasAvg);
-if(dolphinsAvg > koalasAvg && dolphinsAvg >= 100){
-    console.log("Dolphins Win!")
-} else if (koalasAvg > dolphinsAvg && koalasAvg >= 100) {
-    console.log("Koalas Win!")
-} else if (koalasAvg === dolphinsAvg && koalasAvg >= 100 && dolphinsAvg >=100){
-    console.log("Its a draw!")
-} else {
-    console.log("No one wins the trophy!")
-}
+// let dolphinsAvg = (96 + 108 + 89)/3;
+// let koalasAvg = (88 + 100 +  110)/3;
+// console.log(dolphinsAvg);
+// console.log(koalasAvg);
+// if(dolphinsAvg > koalasAvg && dolphinsAvg >= 100){
+//     console.log("Dolphins Win!")
+// } else if (koalasAvg > dolphinsAvg && koalasAvg >= 100) {
+//     console.log("Koalas Win!")
+// } else if (koalasAvg === dolphinsAvg && koalasAvg >= 100 && dolphinsAvg >=100){
+//     console.log("Its a draw!")
+// } else {
+//     console.log("No one wins the trophy!")
+// }
+
+// let usersDay = prompt("What is today?");
+//
+// switch(usersDay.toLowerCase()){
+//     case("monday"):
+//         console.log("Mondays are the worst!");
+//         break;
+//     case("tuesday"):
+//         console.log("Tuesdays, they're alright man...");
+//         break;
+//     case("wednesday"):
+//         console.log("Why does wednesday feel like the number 3?");
+//         break;
+//     case("thursday"):
+//         console.log("Almost to friday!");
+//         break;
+//     case("friday"):
+//         console.log("YOU MADE IT THROUGH THE WEEK!")
+//         break;
+//     default:
+//         console.log("You must be having fun on your weekend!")
+// }
+// let age = prompt("How old are you?")
+//
+// //Long version of ternary operator
+// // age >= 18 ? console.log("I drink a lot of beer") : console.log("I need to wait to drink alcohol!");
+//
+//
+// console.log(`I like to drink ${age >= 18 ?"beer" : "water!"} because im ${age} years old!`)
+
+let bill = parseInt(prompt("How much was the bill value?"));
+let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+console.log(`The bill was ${bill}, the tip is ${tip}, and the total is ${bill + tip}`);
+
