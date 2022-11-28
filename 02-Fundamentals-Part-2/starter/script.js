c4fc023c046b861434b253796ad259594227a14c
Nazareth-1 <nazarethbriseno@gmail.com>
2022-11-27 18:28:24 -0600

    Completed Arrays Lecture

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -6,33 +6,89 @@
 // if (passTest) hasDriversLicense = true;
 // if(hasDriversLicense) console.log('I can drive!');
 
-function logger() {
-    console.log(`My name is Nazareth!`);
+// function logger() {
+//     console.log(`My name is Nazareth!`);
+// }
+//
+// //To "run" a function, it could also be called "calling the function", "running the function" or "invoking the function"
+// logger();
+//
+// function fruitProcessor(apples, oranges){
+//     // console.log(apples, oranges);
+//     const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//     return juice;
+// }
+//
+// const appleJuice = fruitProcessor(5, 0);
+// console.log(appleJuice);
+//
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+//
+// //Arrow function 1 parameter
+// let calcAge = birthYear => 2037 - birthYear;
+// console.log(calcAge(1991));
+//
+// let yearsUntilRetirement = (birthyear, firstName) => {
+//     let age = 2037 - birthyear;
+//     let retirement = 65 - age;
+//     return `${firstName} will retire in ${retirement} years!`;
+// }
+// yearsUntilRetirement(1999);
+
+// let calcAverage = (score1, score2, score3) => (score1 + score2 + score3) / 3;
+// let dolphinsAvg = calcAverage(80, 135, 100);
+// let koalasAvg = calcAverage(65, 54, 49);
+// function checkWinner (avgDolphins, avgKoalas){
+//     if(avgDolphins >= avgKoalas * 2 || avgKoalas >= avgDolphins *2){
+//         if(avgDolphins > avgKoalas){
+//             console.log(`The Dolphins win! (${avgDolphins} VS. ${avgKoalas})`)
+//         } else {
+//             console.log(`The Koalas win! (${avgDolphins} VS. ${avgKoalas})`)
+//         }
+//     } else {
+//         console.log(`No one Wins! (${avgDolphins} VS. ${avgKoalas})`)
+//     }
+// }
+// checkWinner(dolphinsAvg, koalasAvg);
+// checkWinner(576, 111);
+//
+// //Test 2
+// let dolphinsAvg2 = calcAverage(85, 54, 41);
+// let koalasAvg2 = calcAverage(23, 34, 27);
+//
+// checkWinner(dolphinsAvg2, koalasAvg2);
+//
+// let bill = parseInt(prompt("How much was the bill value?"));
+//
+// function calcTip(bill){
+//     let tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.20;
+//     return tip;
+// }
+//
+// let tips = [calcTip(125), calcTip(555), calcTip(44)];
+// let totals = []
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Gonzales',
+    birthYear: 1999,
+    job: 'teacher',
+    friends: ['Minato', 'Rabago', 'Kenny', 'Lewis'],
+    //This is a method!!!!!!
+    calcAge: function(birthYear){
+        return 2037-birthYear;
+    }
 }
 
-//To "run" a function, it could also be called "calling the function", "running the function" or "invoking the function"
-logger();
 
-function fruitProcessor(apples, oranges){
-    // console.log(apples, oranges);
-    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-    return juice;
-}
-
-const appleJuice = fruitProcessor(5, 0);
-console.log(appleJuice);
-
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
-
-//Arrow function 1 parameter
-let calcAge = birthYear => 2037 - birthYear;
-console.log(calcAge(1991));
-
-let yearsUntilRetirement = (birthyear, firstName) => {
-    let age = 2037 - birthyear;
-    let retirement = 65 - age;
-    return `${firstName} will retire in ${retirement} years!`;
-}
-yearsUntilRetirement(1999);
-
+// let interestedIn = prompt("What would you like to know about Jonas? Please enter firstName, lastName, age or job!")
+//
+// if(jonas[interestedIn]){
+//     alert(jonas[interestedIn]);
+// } else {
+//     alert("Sorry we don't know that about Jonas!")
+// }
+//
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friends name is ${jonas.friends[0]}!`);
+console.log(jonas.calcAge(1999));
